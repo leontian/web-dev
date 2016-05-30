4bd1111a7b6581e0a316c9f90bc5b7d22f04bec4
leontian <l.tian.email@gmail.com>
2016-05-30 13:16:21 -0400

    angular routing for user views

--- public/assignment/config.js
+++ public/assignment/config.js
@@ -1,3 +1,23 @@
 /**
  * Created by leon on 5/30/16.
  */
+(function () {
+    angular
+        .module("WebAppMaker")
+        .config(Config);
+    
+    function Config($routeProvider) {
+        $routeProvider
+            .when("/profile", {
+                templateUrl: "views/user/profile.view.client.html"
+            })
+            .when("/register", {
+                templateUrl: "views/user/register.view.client.html"
+            })
+            .when("/login", {
+                templateUrl: "views/user/login.view.client.html"
+            });
+        
+    }
+    
+})();
