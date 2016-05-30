7e07089f83c705cc21f4ed7fe70aec8f3f48d002
leontian <l.tian.email@gmail.com>
2016-05-30 14:50:13 -0400

    basic login url redirect, with error message

--- public/assignment/views/user/login.controller.client.js
+++ public/assignment/views/user/login.controller.client.js
@@ -6,11 +6,27 @@
         .module("WebAppMaker")
         .controller("LoginController", LoginController);
     
-    function LoginController() {
+    function LoginController($location) {
         var vm = this;
+        var users =
+            [
+                {_id: "123", username: "alice",    password: "alice",    firstName: "Alice",  lastName: "Wonder"  },
+                {_id: "234", username: "bob",      password: "bob",      firstName: "Bob",    lastName: "Marley"  },
+                {_id: "345", username: "charly",   password: "charly",   firstName: "Charly", lastName: "Garcia"  },
+                {_id: "456", username: "jannunzi", password: "jannunzi", firstName: "Jose",   lastName: "Annunzi" }
+            ];
         
-        vm.login = function() {
-            console.log(vm.username);
+        vm.login = function(username, password) {
+            for (var i in users) {
+                if (users[i].username === username && users[i].password === password) {
+                    console.log("yay");
+                    $location.url("/profile");
+                }
+
+                else {
+                    vm.error = "User not found.";
+                }
+            }
         }
     }
 })();
