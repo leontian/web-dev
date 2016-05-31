be484b83948711762dea408108fa04174f06ba6a
leontian <l.tian.email@gmail.com>
2016-05-31 16:13:50 -0400

    all routes

--- public/assignment/config.js
+++ public/assignment/config.js
@@ -8,6 +8,26 @@
     
     function Config($routeProvider) {
         $routeProvider
+            //widgets
+            .when("/user/:uid/website/:wid/page/:pid/widget/new", {
+                templateUrl: "views/website/widget-new.view.client.html"
+            })
+            .when("/user/:uid/website/:wid/page/:pid/widget/:wgid", {
+                templateUrl: "views/website/widget-edit.view.client.html"
+            })
+            .when("/user/:uid/website/:wid/page/:pid/widget", {
+                templateUrl: "views/website/widget-list.view.client.html"
+            })
+            //pages
+            .when("/user/:uid/website/:wid/page/new", {
+                templateUrl: "views/website/page-new.view.client.html"
+            })
+            .when("/user/:uid/website/:wid/page/:pid", {
+                templateUrl: "views/website/page-edit.view.client.html"
+            })
+            .when("/user/:uid/website/:wid/page", {
+                templateUrl: "views/website/page-list.view.client.html"
+            })
             // will check if the param after website is "new" first
             // order matters
             .when("/user/:uid/website/new", {
