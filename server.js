8bc1602650e291bfb9e7576801e96d34675102d1
leontian <l.tian.email@gmail.com>
2016-05-31 00:55:58 -0400

    port fix and README

--- server.js
+++ server.js
@@ -11,6 +11,8 @@ app.use(express.static(__dirname + '/public'));
 require ("./test/app.js")(app);
 
 var ipaddress = process.env.OPENSHIFT_NODEJS_IP;
-var port      = process.env.OPENSHIFT_NODEJS_PORT || 1337;
+var port      = process.env.OPENSHIFT_NODEJS_PORT || 3000;
+console.log(ipaddress);
+console.log(port);
 
 app.listen(port, ipaddress);
