d10218ba7ec8bba651d4ec130dd6ac7b3a27512e
leontian <l.tian.email@gmail.com>
2016-06-02 21:43:26 -0400

    widget web services

--- assignment/services/widget.service.server.js
+++ assignment/services/widget.service.server.js
@@ -1,3 +1,81 @@
 /**
  * Created by leon on 6/2/16.
  */
+module.exports = function (app) {
+
+    var widgets = [
+        { "_id": "123", "widgetType": "HEADER", "pageId": "321", "size": 2, "text": "GIZMODO"},
+        { "_id": "234", "widgetType": "HEADER", "pageId": "321", "size": 4, "text": "Lorem ipsum"},
+        { "_id": "345", "widgetType": "IMAGE", "pageId": "321", "width": "100%",
+            "url": "http://lorempixel.com/400/200/"},
+        { "_id": "456", "widgetType": "HTML", "pageId": "321", "text": "<p>Lorem ipsum</p>"},
+        { "_id": "567", "widgetType": "HEADER", "pageId": "321", "size": 4, "text": "Lorem ipsum"},
+        { "_id": "678", "widgetType": "YOUTUBE", "pageId": "321", "width": "100%",
+            "url": "https://youtu.be/AM2Ivdi9c4E" },
+        { "_id": "789", "widgetType": "HTML", "pageId": "321", "text": "<p>Lorem ipsum</p>"}
+    ];
+
+    app.post("/api/website/:websiteId/widget", createWidget);
+    app.get("/api/website/:websiteId/widget", findAllWidgetsForPage);
+    // TODO: make sure widgetId unique in db
+    app.get("/api/widget/:widgetId", findWidgetById);
+    app.put("/api/widget/:widgetId", updateWidget);
+    app.delete("/api/widget/:widgetId", deleteWidget);
+
+    function createWidget(req, res) {
+        var widget = req.body;
+        widgets.push(widget);
+        res.send(200);
+    }
+
+    function findAllWidgetsForPage(req, res) {
+        var websiteId = req.params.websiteId;
+        var result = [];
+        for (var i in widgets) {
+            if (widgets[i].websiteId === websiteId) {
+                result.push(widgets[i]);
+            }
+        }
+        res.json(result);
+    }
+
+    function findWidgetById(req, res) {
+        var widgetId = req.params.widgetId;
+        for (var i in widgets) {
+            if (widgets[i]._id === widgetId) {
+                res.send(widgets[i]);
+                return;
+            }
+        }
+        res.json({});
+
+    }
+
+    function updateWidget(req, res) {
+        var widget = req.body;
+        for (var i in widgets) {
+            if (widgets[i]._id === user._id) {
+                widgets[i] = widget;
+                res.send(200);
+                return;
+            }
+        }
+        res.send(400);
+
+    }
+
+    function deleteWidget(req, res) {
+        var id = req.params.widgetId;
+        for (var i in widgets) {
+            if (widgets[i]._id === id) {
+                widgets.splice(i, 1);
+                res.send(200);
+                return;
+            }
+        }
+        res.send(400);
+    }
+
+
+
+};
